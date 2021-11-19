e9fd420dd97069f4b8f2aac9201ec9f119a057cc
jinicha <jinichaa@gmail.com>
2021-11-19 00:22:07 -0800

    Search for flights with one stop if no non-stop flights are found

--- flight-deals-end/flight_search.py
+++ flight-deals-end/flight_search.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 import config
 from flight_data import FlightData
 
@@ -41,7 +42,24 @@ class FlightSearch:
         try:
             data = response.json()["data"][0]
         except IndexError:
-            print(f"No flights found for {destination_city_code}.")
+            print(f"No flights found for {destination_city_code}. Looking for flights with one stop...")
+            res = requests.get(
+                url=f"{TEQUILA_ENDPOINT}/v2/search",
+                headers=headers,
+                params={
+                    "fly_from": origin_city_code,
+                    "fly_to": destination_city_code,
+                    "date_from": from_time.strftime("%d/%m/%Y"),
+                    "date_to": to_time.strftime("%d/%m/%Y"),
+                    "nights_in_dst_from": 7,
+                    "nights_in_dst_to": 28,
+                    "flight_type": "round",
+                    "one_for_city": 1,
+                    "max_stopovers": 1,
+                    "curr": "USD"
+                }
+            )
+            pprint(res.json())
             return None
 
         flight_data = FlightData(
